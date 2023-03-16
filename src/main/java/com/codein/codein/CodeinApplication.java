dfdeaf476bea297f3bbadc252a3661b4e62d7c0f
Dale <kdha4585@gmail.com>
2023-03-16 14:49:11 +0900

    first commit

---
+++ src/main/java/com/codein/codein/CodeinApplication.java
@@ -0,0 +1,13 @@
+package com.codein.codein;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codein/codein/CodeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codein.codein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
