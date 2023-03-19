95d8493852ac41c89e47203f7d4a942a747db156
Dale <kdha4585@gmail.com>
2023-03-19 14:57:07 +0900

    rollback

---
+++ codein/src/test/java/com/study/codein/CodeinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.codein;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
