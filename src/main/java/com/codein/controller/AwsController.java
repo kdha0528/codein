f6b91ab5e48baebc2b82d941a7f8191f36a9475d
Dale <kdha4585@gmail.com>
2023-08-09 09:11:09 +0900

    fix: checkHealthStatus 컨트롤러 추가
    로드밸런서 라우팅에 대해 200 status를 반환하기 위한
    checkHealthStatus 컨트롤러 추가

---
+++ src/main/java/com/codein/controller/AwsController.java
@@ -0,0 +1,18 @@
+package com.codein.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AwsController {
+
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> checkHealthStatus() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
